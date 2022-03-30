1c77722e2acbce870f23289d4aba1be1723e5aac
Kyle Kotowick <kotowick@invictonlabs.com>
2022-03-30 18:56:32 -0400

    Switch to using a pre-archived ZIP file

--- lambda.tf
+++ lambda.tf
@@ -12,13 +12,12 @@ data "aws_iam_policy_document" "iterator" {
 
 // The function that calls the function of interest
 module "lambda_iterator" {
-  source                   = "Invicton-Labs/lambda-set/aws"
-  version                  = "~> 0.4.2"
-  edge                     = false
-  source_directory         = "${path.module}/lambdas/iterator"
-  archive_output_directory = "${path.module}/archives/"
+  source  = "Invicton-Labs/lambda-set/aws"
+  version = "~> 0.4.2"
+  edge    = false
   lambda_config = {
     function_name                  = "subminute-lambda-event-iterator-${random_id.id.hex}"
+    filename                       = "${path.module}/iterator.zip"
     timeout                        = 2
     memory_size                    = 128
     handler                        = "main.lambda_handler"
